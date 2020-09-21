f9ee779463b72624a766e020043dea2ffedccfcd
vincy <vincy.emmanuelli@live.com>
2020-09-21 00:55:29 -0400

    update tres heroku

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql"); 
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
+    host: "d1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "fa4svypra3fxljo2	",
+    password: "gmeawz49ybyhrtt0",
     database: "burgers_db" 
   });
 };
 
-
 // Make connection.
 connection.connect();
-
-  // if (err) {
-  //   console.error("error connecting: " + err.stack);
-  //   return;
-  // }
-  // console.log("connected as id " + connection.threadId);
 
 // Export connection for our ORM to use.
 module.exports = connection;
